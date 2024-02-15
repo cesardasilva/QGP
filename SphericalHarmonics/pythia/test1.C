1b740ddfc153dfc82a39ac83265cef531578ce5e
Cesar Luiz da Silva <cesar_luiz@lanl.gov>
2024-02-15 15:30:45 -0700

    Saves histogram
    Code which reads the Pythia generated tuple and makes a data vs. phi histogram.

--- SphericalHarmonics/pythia/test1.C
+++ SphericalHarmonics/pythia/test1.C
@@ -1,9 +1,10 @@
 void test1()
 {
   TFile *f = TFile::Open("pytree.root");
-  TH3F *T; f->GetObject("T",T);
-
-  T -> Draw("eta:phi>>h(100,-5,5,-3.1415,3.1415)","","colz");
-  
+  TH2F* hdphi_deta = new TH2F("hdphi_deta","; #Delta#phi; #Delta#eta", 100, -5, 5, 100, -6.283185307,6.283185307);
+  T -> Draw("eta:phi>>hdphi_deta","","goff");
+  TFile* fout = new TFile("pythia_histos.root");
+  hdphi_deta->Write();
+  fout->Close();
 }
 
